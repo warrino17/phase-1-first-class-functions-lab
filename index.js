f74d36dd1d425aa44a5c414a86a26e7a27eb1453
Warren Zhang <warrenzhang17@gmail.com>
2023-03-21 16:57:26 -0400

    i hate driving!

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers) {
+   return drivers.slice(0,2);
+}
+
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+];
+
+const createFareMultiplier = function(value) {
+    return function(fare) {
+        return fare * value;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, selectingDrivers) {
+    return selectingDrivers(drivers);
+}
